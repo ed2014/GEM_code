1040b9dd72c55596e15c717252d069bc60816751
unknown <Cfleit@LIN-L-426055.PFR.CO.NZ>
2015-07-24 23:30:34 +1200

    minor changes

--- CreateSims_GEM.R
+++ CreateSims_GEM.R
@@ -15,7 +15,7 @@ climates <- c("Base\\")                                                         
 sowDate <- c("-sep","-oct","-nov","-dec","-jan")   # Define months (-mmm) Obs: it assumes dates later in the scripr FIXME: Quick solution to be improved
 
 # select met files to define which .sims to produce
-#metFiles <- c("67_180.met", "189_60.met", "228_80.met")                           # Option 1: select pixels (row_column) from met files
+#metFiles <- c("67_180.met", "189_60.met", "228_80.met") # Option 1: select pixels (row_column) from met files
 load("C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\gisLayers\\luFilter\\LandUseArableFilter_metList.RData", .GlobalEnv)    # Option 2: gets a list of grid-cell/files selected (LU layer in this case)
 
 metFiles <- metList
@@ -26,25 +26,22 @@ hybrids <- c("h1","h2", "h3","h4","h5")                                         
 p1 <-  c(120,145,170,195,220) # parameter values for emergence to end of juvenile
 p2 <- c(850,875,900,925,950) # parameter values for flowering to maturity
 
-
-
 # get data from the root (base) .sim file
 #doc = xmlInternalTreeParse(rootSimFile)
-doc = xmlTreeParse(rootSimFile, useInternalNodes = TRUE)
+doc <- xmlTreeParse(rootSimFile, useInternalNodes = TRUE)
 #doc = xmlTreeParse(rootSimFile, getDTD = FALSE, useInternalNodes = TRUE)
 
 # Select all nodels to be modified in sim files
-simNameRoot = xmlRoot(doc)
-nodesMet = getNodeSet(doc, "//filename") 
-nodesOut = getNodeSet(doc, "//outputfile")
-nodesStDate = getNodeSet(doc, "//start_date")
-nodesEnDate = getNodeSet(doc, "//end_date")
-nodesStSow = getNodeSet(doc, "//StartSow")[1] # check if the crop of interest is the first in occurrence (number in bracket)
-nodesEnSow = getNodeSet(doc, "//EndSow")[1]   # 
-nodesEnRot = getNodeSet(doc, "//EndRot")[1]   # 
-nodesP1 = getNodeSet(doc, "//medium/tt_emerg_to_endjuv")   # 
-nodesP2 = getNodeSet(doc, "//medium/tt_flower_to_maturity")   # 
-
+simNameRoot <- xmlRoot(doc)
+nodesMet <- getNodeSet(doc, "//filename") 
+nodesOut <- getNodeSet(doc, "//outputfile")
+nodesStDate <- getNodeSet(doc, "//start_date")
+nodesEnDate <- getNodeSet(doc, "//end_date")
+nodesStSow <- getNodeSet(doc, "//StartSow")[1] # check if the crop of interest is the first in occurrence (number in bracket)
+nodesEnSow <- getNodeSet(doc, "//EndSow")[1]   # 
+nodesEnRot <- getNodeSet(doc, "//EndRot")[1]   # 
+nodesP1 <- getNodeSet(doc, "//medium/tt_emerg_to_endjuv")   # 
+nodesP2 <- getNodeSet(doc, "//medium/tt_flower_to_maturity")   # 
 
 # Loops through climate scenarios "folders"
 for(cl in 1:length(climates)){
@@ -72,14 +69,14 @@ for(cl in 1:length(climates)){
   
   #loop through met file names to create one .sim file for each met file
 
-   # for(s in seq_along(sowDate)) {
-      for(s in 4:5) {
-   #   for(h in seq_along(hybrids)) {
-        for(h in 3:5) {
+    for(s in seq_along(sowDate)) {
+      #for(s in 4:5) {
+      for(h in seq_along(hybrids)) {
+       # for(h in 3:5) {
         for(i in seq_along(metFiles)) {
           
     # Create out folder for this scenario
-    outDir = paste0(simFolder,"h",h,"_s",s)
+    outDir <- paste0(simFolder,"h",h,"_s",s)
     suppressWarnings(dir.create(outDir))
           
     # For individual pixels  
@@ -87,47 +84,47 @@ for(cl in 1:length(climates)){
     #outName = paste0(simName,".out")
     
     # For list of met files
-    splitName = unlist(strsplit(metFiles[i],"[.]"))
-    thisSim = paste0(splitName[1],"_", hybrids[h],"_s", s)
-    simName = paste0(thisSim,".sim")
-    outName = paste0(thisSim,".out")
+    splitName <- unlist(strsplit(metFiles[i],"[.]"))
+    thisSim <- paste0(splitName[1],"_", hybrids[h],"_s", s)
+    simName <- paste0(thisSim,".sim")
+    outName <- paste0(thisSim,".out")
 
     # change attribute name of simulation 
-    xmlAttrs(simNameRoot) = c(name = thisSim)
+    xmlAttrs(simNameRoot) <- c(name = thisSim)
     
     #  find address to point out to right met files
-    newMetNode = paste0(metFolder,metFiles[i])
+    newMetNode <- paste0(metFolder,metFiles[i])
     
     # change met location
     lapply(nodesMet, function(n) {
-      xmlValue(n) = newMetNode
+      xmlValue(n) <- newMetNode
     })
     
     # change outfile name
     lapply(nodesOut, function(n) {
-      xmlValue(n) = outName
+      xmlValue(n) <- outName
     })
     
     # change sowing dates
     lapply(nodesStSow, function(n) {
-      xmlValue(n) = paste0(1,sowDate[s]) # just assumes starting with 1st day and moving a day ahead for others
+      xmlValue(n) <- paste0(1,sowDate[s]) # just assumes starting with 1st day and moving a day ahead for others
     })
     lapply(nodesEnSow, function(n) {
-      xmlValue(n) = paste0(2,sowDate[s])
+      xmlValue(n) <- paste0(2,sowDate[s])
     })
     lapply(nodesEnRot, function(n) {
-      xmlValue(n) = paste0(3,sowDate[s])
+      xmlValue(n) <- paste0(3,sowDate[s])
     })
     # change parameter values
     lapply(nodesP1, function(n) {
-      xmlValue(n) = p1[h]
+      xmlValue(n) <- p1[h]
     })
     lapply(nodesP2, function(n) {
-      xmlValue(n) = p2[h]
+      xmlValue(n) <- p2[h]
     })
     
     # FIXME: identation of saved XML file is corrupted when it has text but no problem with functionality
-    saveXML(doc, file = paste0(outDir,"\\",simName),  indent=TRUE)
+    saveXML(doc, file <- paste0(outDir,"\\",simName),  indent=TRUE)
     }   # end pixel 
    }  # end hybrid
   } # end sow date
