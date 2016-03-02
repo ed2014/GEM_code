1d747cc2d69236858a4777c344d0f167034a1719
unknown <cfleit@LIN-L-427902.PFR.CO.NZ>
2016-03-03 10:58:44 +1300

    set up new p1 and p2 param for last run, sow on the first of each month

--- CreateSims_GEM.R
+++ CreateSims_GEM.R
@@ -1,28 +1,44 @@
 # Read a base sim file and create it for othe pixels
+# 2 Mar 2016 New runs for specific machines MyRun folder
 
 # reads a "base" sim file (XML) finds out a list of met files to run sim files to run these met files
-library("ncdf")
+#library("ncdf")
 library(XML)
+#library("ncdf4")
 
 # Parameters and paths
 gc()
 
 # Select folders and file locations
-metFolder <- "\\\\simplace.net\\projects\\nz\\weatherHistorical\\metFiles\\"    # set folder locations (comment the ones not used)
-metFolder2 <- "F:\\ERA-40_Reanalysis\\metFiles_ERA_1971_2000\\"
+# metFolder <- "C:\\MyRun\\MetFiles"
+metFolder <- "C:\\MyRun\\MetFilesTest\\"
+simFolder <- "C:\\MyRun\\SimFiles\\"
+rootSimFile <- "C:\\GitHubRepos\\GEM_code\\baseSim\\BaseSim.sim" # find base simulation 
+climates <- c("Base\\") # Define climate scenarios to run as different folders
+sowDate <- c("-sep","-oct","-nov","-dec","-jan")  
+hybrids <- c("h1","h2", "h3","h4", "h5")                                               # select hybrids
+#p1 <-  c(150,185,220,255,290) # parameter values for emergence to end of juvenile
+p1 <- c(130,160,190,220,250) 
+p2 <- c(850,875,900,925,950) # parameter values for flowering to maturity
+#p2 <- c(850,850,850,850,850) # test fixed p2 - NO EFFECT
+ 	  
+
+# metFolder <- "\\\\simplace.net\\projects\\nz\\weatherHistorical\\metFiles\\"    # set folder locations (comment the ones not used)
+#metFolder2 <- "F:\\ERA-40_Reanalysis\\metFiles_ERA_1971_2000\\"
 #metFolder <- "C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\metFiles\\testPixels\\"
 #simFolder <- "Z:\\simFiles\\"    # sim folder to save new .sims
-simFolder <- "F:\\SowByGenotype\\simsRun2\\"    # sim folder to save new .sims
+#simFolder <- "F:\\SowByGenotype\\simsRun2\\"    # sim folder to save new .sims
 #simFolder <- "C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\simFiles\\"
-rootSimFile <- "C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\GEM_code\\baseSim\\BaseSim.sim" # find base simulation
-climates <- c("Base\\")                                                                # Define climate scenarios to run as different folders
+#rootSimFile <- "C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\GEM_code\\baseSim\\BaseSim.sim" # find base simulation
+#climates <- c("Base\\")                                                                # Define climate scenarios to run as different folders
 #sowDate <- c("-sep","-oct","-nov","-dec","-jan")   # Define months (-mmm) Obs: it assumes dates later in the scripr FIXME: Quick solution to be improved
-sowDate <- c("-sep","-oct","-nov","-dec")
+#sowDate <- c("-sep","-oct","-nov","-dec")
 
 # select met files to define which .sims to produce
 # finds out met files to point to
 metFiles <- NULL
-metFiles <- list.files(metFolder2,pattern='.met', full.names=FALSE) # Option 1: gets all met files in a folder
+#metFiles <- list.files(metFolder2,pattern='.met', full.names=FALSE) # Option 1: gets all met files in a folder
+metFiles <- list.files(metFolder,pattern='.met', full.names=FALSE) # Option 1: gets all met files in a folder
 #load("C:\\Apsim_dev\\Projects\\2014-SowByGenotype\\gisLayers\\luFilter\\LandUseArableFilter_metList.RData", .GlobalEnv)    # Option 2: gets a list of grid-cell/files selected (LU layer in this case)
 # metFiles <- metList
 #metFiles <- c("10_135.met" , "100_148.met")
@@ -32,9 +48,7 @@ print(paste0("Found ", length(metFiles), " .met files"))
 #hybrids <- c("h1","h2", "h3","h4","h5")                                               # select hybrids
 #p1 <-  c(120,145,170,195,220) # parameter values for emergence to end of juvenile
 #p2 <- c(850,875,900,925,950) # parameter values for flowering to maturity
-hybrids <- c("h1","h2", "h3","h4")                                               # select hybrids
-p1 <-  c(120,153,187,220) # parameter values for emergence to end of juvenile
-p2 <- c(850,883,917,950) # parameter values for flowering to maturity
+
      
 # get data from the root (base) .sim file
 #doc = xmlInternalTreeParse(rootSimFile)
@@ -59,8 +73,7 @@ for(cl in 1:length(climates)){
   # choose dates for the current climate scenario
   if(climates[cl] == "Base\\") {
     
-   # stDate <- "01/01/1971"
-    stDate <- "01/01/1980"
+    stDate <- "01/01/1971"
     enDate <- "30/12/2000"
     
   } else {
@@ -118,13 +131,13 @@ for(cl in 1:length(climates)){
     
     # change sowing dates
     lapply(nodesStSow, function(n) {
-      xmlValue(n) <- paste0(15,sowDate[s]) # just assumes starting with 1st (or 15th) day and moving a day ahead for others
+      xmlValue(n) <- paste0(1,sowDate[s]) # just assumes starting with 1st (or 15th) day and moving a day ahead for others
     })
     lapply(nodesEnSow, function(n) {
-      xmlValue(n) <- paste0(16,sowDate[s])
+      xmlValue(n) <- paste0(2,sowDate[s])
     })
     lapply(nodesEnRot, function(n) {
-      xmlValue(n) <- paste0(17,sowDate[s])
+      xmlValue(n) <- paste0(3,sowDate[s])
     })
     # change parameter values
     lapply(nodesP1, function(n) {
